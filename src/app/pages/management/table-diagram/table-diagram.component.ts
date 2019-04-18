253fe4c6b327211882e69221d414621e8eaafb6a
Pham Van Tai <tai.phamvan@dallmeier.com>
2019-04-18 10:42:29 +0700

    change css to scss

--- src/app/pages/management/table-diagram/table-diagram.component.ts
+++ src/app/pages/management/table-diagram/table-diagram.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-table-diagram',
   templateUrl: './table-diagram.component.html',
-  styleUrls: ['./table-diagram.component.css']
+  styleUrls: ['./table-diagram.component.scss']
 })
 export class TableDiagramComponent implements OnInit {
 
